2f0ff90a5245d959c2f2ba8523f10d99972e55a7
Evgeniy Amiraslanov <the.modifikator@gmail.com>
2022-06-20 01:09:22 +0300

    Add font preview

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,8 @@
   import Result from './lib/Result.svelte';
   import ComparisonToggle from './lib/ComparisonToggle.svelte';
 
+  const PREVIEW_TEXT = 'The fox jumped over the lazy dog, the scoundrel.';
+
   let commonProps = {
     fontSize: 16,
     fontWeight: 200,
@@ -67,14 +69,22 @@
 </svelte:head>
 
 <main class="cls-main">
-  <div class="primary">
-    <PrimaryFontSelector bind:value={primaryFontFamily} />
-    <CommonForm bind:value={commonProps} />
-  </div>
-  <div class="fallback">
-    <FallbackFontSelector bind:value={fallbackFontFamily} />
-    <SpacingForm bind:value={fallbackSpacing} />
-    <FModsBothForm bind:value={fallbackFontProps} />
+  <div class="controls">
+    <div class="primary">
+      <PrimaryFontSelector bind:value={primaryFontFamily} />
+      <CommonForm bind:value={commonProps} />
+    </div>
+    <div class="fallback">
+      <FallbackFontSelector bind:value={fallbackFontFamily} />
+      <SpacingForm bind:value={fallbackSpacing} />
+      <FModsBothForm bind:value={fallbackFontProps} />
+    </div>
+    <div class="primary-preview"
+         style:font-weight={commonProps.fontWeight}
+         style:font-family={primaryFontFamily}>{PREVIEW_TEXT}</div>
+    <div class="fallback-preview"
+         style:font-weight={commonProps.fontWeight}
+         style:font-family={fallbackFontFamily}>{PREVIEW_TEXT}</div>
   </div>
   <div class="comparison">
     <ComparisonToggle bind:showComparison={showComparison} />
@@ -100,12 +110,19 @@
     flex: 0 1 1300px;
   }
 
-  .primary,
-  .fallback {
-    flex: 0 0 auto;
-    align-self: flex-start;
+  .controls {
+    flex: 0 1 auto;
     position: sticky;
     top: 0;
+    align-self: flex-start;
+    display: grid;
+    grid-template-columns: 20rem 20rem;
+    grid-column-gap: 2rem;
+  }
+
+  .primary-preview,
+  .fallback-preview {
+    font-size: 1.5rem;
   }
 
   .comparison {
